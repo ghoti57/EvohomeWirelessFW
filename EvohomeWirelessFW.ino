ae3b758257e6af9fbd43697c71d822e2853f534a
ghoti57 <ghoti57@gmail.com>
2020-04-12 16:26:31 +0100

    Use correct edge of GDO2 clock for sampling and setting data bits

--- EvohomeWirelessFW.ino
+++ EvohomeWirelessFW.ino
@@ -311,7 +311,7 @@ void loop() {
     circ_buffer.push(0x35,true);
     pinMode(2,INPUT);
     while(((CCx.Write(CCx_SRX,0)>>4)&7)!=1); 
-    attachInterrupt(GDO2_INT, sync_clk_in, FALLING);
+    attachInterrupt(GDO2_INT, sync_clk_in, RISING);
     pp=0;
     op=0;
     sp=0;
@@ -557,6 +557,6 @@ void loop() {
        pp=0;
        out_flags=0;//reuse for preamble counter
        circ_buffer.push(0x53,true); //don't push anything while interrupt is running
-       attachInterrupt(GDO2_INT, sync_clk_out, RISING);
+       attachInterrupt(GDO2_INT, sync_clk_out, FALLING);
   }
 }
